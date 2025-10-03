da0617cb0272f6d881cc6f07e5105efaa8458d0e
Rune Harlyk <rune@harlyk.dk>
2025-10-03 20:43:23 +0200

    Adds folder template for exercises

---
+++ PROBLEM_BASE/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const double EPS = 1e-9;
+const int MAXN = 2e5 + 5;
+
+int main() {
+  fast_io;
+
+  return 0;
+}
